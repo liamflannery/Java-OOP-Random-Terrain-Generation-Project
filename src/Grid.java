c31635f5587f290baddcc60eee1674421b55b01c
liamflannery <55472933+liamflannery@users.noreply.github.com>
2021-08-19 17:00:48 +1000

    Added water, buildings, road

--- src/Grid.java
+++ src/Grid.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 
 class Grid {
     Cell[][] cells = new Cell[20][20];
-    ArrayList<String> doNotReplace = new ArrayList<String>();
+    ArrayList<String> doNotReplaceMT = new ArrayList<String>();
+    ArrayList<String> doNotReplaceRS = new ArrayList<String>();
+    ArrayList<String> doNotReplaceRo = new ArrayList<String>();
 
     public Grid(){
-        doNotReplace.add("MountainTop");
+        doNotReplaceMT.add("MountainTop");
+        doNotReplaceRS.add("MountainTop");
+        doNotReplaceRS.add("Mountain");
+        doNotReplaceRo.add("Mountain");
+        doNotReplaceRo.add("MountainTop");
+        doNotReplaceRo.add("Water");
+        doNotReplaceRo.add("WaterSource");
+
         setCell();
     }
 
@@ -29,13 +38,48 @@ class Grid {
         //Form mountains around the peaks
        for(int i = 0; i < cells.length; i++){
             for(int j = 0; j < cells[i].length; j++){
-                if(doNotReplace.contains(cells[i][j].getClass().getName()) != true){
+                if(doNotReplaceMT.contains(cells[i][j].getClass().getName()) != true){
                     cells[i][j] = mountainCont(i,j);
                 }
                 
             }
         }
-       
+        //Add in river source cells
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                if(doNotReplaceRS.contains(cells[i][j].getClass().getName()) != true){
+                    cells[i][j] = riverSource(i,j);
+                }
+                
+            }
+        }
+        //Add in rivers
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                if(doNotReplaceRS.contains(cells[i][j].getClass().getName()) != true){
+                    cells[i][j] = addRiver(i,j);
+                }
+                
+            }
+        }
+        //Add in buildings
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                if(doNotReplaceRS.contains(cells[i][j].getClass().getName()) != true){
+                    cells[i][j] = addBuildings(i,j);
+                }
+                
+            }
+        }
+        //Add in roads
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                if(doNotReplaceRo.contains(cells[i][j].getClass().getName()) != true){   
+                    cells[i][j] = addRoad(i,j);
+                }
+                
+            }
+        }
     }
     private Cell mountainTop(int x, int y){
         double typeSelector = Math.random();
@@ -68,7 +112,42 @@ class Grid {
             return new Grass(10 + 35 * x, 10 + 35 * y);
         }
     }
-    
+    private Cell riverSource(int x, int y){
+        double typeSelector = Math.random();
+        if(surroundingContains(x, y, "MountainTop") == false && surroundingContains(x, y, "WaterSource") == false&& typeSelector < 0.02){
+            return new WaterSource(10 + 35 * x, 10 + 35 * y);
+        }
+        else{
+            return cells[x][y];
+        }
+    }
+    private Cell addRiver(int x, int y){
+        double typeSelector = Math.random();
+        if(surroundingContains(x, y, "MountainTop") == false){
+            if(below(x, y, "WaterSource") | below(x,y, "Water")){
+                return new Water(10 + 35 * x, 10 + 35 * y);
+            }
+            else{
+                return cells[x][y];
+            }
+            
+        }
+        else{
+            return cells[x][y];
+        }
+    }
+    private Cell addRoad(int x, int y){
+        double typeSelector = Math.random();
+        if(surroundingContains(x, y, "Building") && typeSelector < 0.5){
+            return new Road(10 + 35 * x, 10 + 35 * y);
+        }
+        if(cellCount(x, y, "Road") == 1){
+            return new Road(10 + 35 * x, 10 + 35 * y);
+        }
+        else{
+            return cells[x][y];
+        }
+    }
     private ArrayList<Cell> surrounding(int x, int y){
         ArrayList<Cell> surroundingCells = new ArrayList<Cell>();
         for(int i = 0; i < 3; i++){
@@ -83,6 +162,15 @@ class Grid {
         }
         return surroundingCells;
     }
+    private Cell addBuildings(int x, int y){
+        double typeSelector = Math.random();
+        if(surroundingContains(x, y, "MountainTop") == false && surroundingContains(x, y, "Water") == false && surroundingContains(x, y, "WaterSource") == false && typeSelector < 0.05){
+            return new Building(10 + 35 * x, 10 + 35 * y);
+        }
+        else{
+            return cells[x][y];
+        }
+    }
     private boolean surroundingContains(int x, int y, String name){
         ArrayList<Cell> surroundingCells = surrounding(x,y);
         for(Cell i: surroundingCells){
@@ -94,7 +182,34 @@ class Grid {
         }
         return false;
     }
-
+    private boolean below(int x, int y, String name){
+        ArrayList<Cell> surroundingCells = surrounding(x,y);
+        for(int i = 2; i < 9; i++){
+            if(i == 2 || i == 5 || i == 8){
+            if(surroundingCells.get(i) != null){
+                if(surroundingCells.get(i).getClass().getName().equals(name)){
+                    return true;
+                }
+            }
+        }
+        }
+        return false;
+    }
+  
+    private int cellCount(int x, int y, String name){
+        ArrayList<Cell> surroundingCells = surrounding(x,y);
+        int cellCount = 0;
+        for(int i = 0; i < 9 ; i++){
+            if(i % 2 != 0){
+                if(surroundingCells.get(i) != null){
+                    if(surroundingCells.get(i).getClass().getName().equals(name)){
+                        cellCount++;
+                    }
+                }
+            }
+        }
+        return cellCount;
+    }
     public void paint(Graphics g, Point mousePos){
         for(int i = 0; i < cells.length; i++){
             for(int j = 0; j < cells[i].length; j++){
